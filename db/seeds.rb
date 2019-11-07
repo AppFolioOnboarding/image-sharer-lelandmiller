ec1b566a98c1a85c5b7b138665b5b1b9c71de9d4
Leland Miller <lelandmiller@gmail.com>
2019-11-07 15:31:03 -0800

    Add seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+images = [].tap do |arr|
+  20.times do
+    arr << { url: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Flower_poster_2.jpg' }
+  end
+end
+
+Image.create(images)
